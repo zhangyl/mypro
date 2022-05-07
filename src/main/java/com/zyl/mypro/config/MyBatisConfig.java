ebb206543e2900d581ca3a9327901b8ff7078d8a
zhangyl <zhangyl@maycur.com>
2022-05-07 16:04:05 +0800

    java代码配置

---
+++ src/main/java/com/zyl/mypro/config/MyBatisConfig.java
@@ -0,0 +1,55 @@
+package com.zyl.mypro.config;
+
+import java.beans.PropertyVetoException;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.zyl.mypro.aop.MybatisInterceptor;
+
+@Configuration
+@MapperScan(value = "com.zyl.mypro.mapper")
+public class MyBatisConfig {
+    @Value("${spring.datasource.driver-class-name}")
+    private String jdbcDriver;
+    @Value("${spring.datasource.url}")
+    private String jdbcUrl;
+    @Value("${spring.datasource.username}")
+    private String jdbcUser;
+    @Value("${spring.datasource.password}")
+    private String jdbcPassword;
+ 
+    @Bean
+    public DataSource createDataSource() throws PropertyVetoException {
+    	DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setDriverClassName(jdbcDriver);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(jdbcUser);
+        dataSource.setPassword(jdbcPassword);
+        return dataSource;
+    }
+    @Bean
+    public DataSourceTransactionManager jdbcTransactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+        sessionFactory.setMapperLocations(resolver.getResources("classpath:mybatis/*Mapper.xml"));
+        sessionFactory.setPlugins(new MybatisInterceptor());
+
+        return sessionFactory.getObject();
+    }
+}
