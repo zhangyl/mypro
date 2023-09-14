f5890d57c1ac57fa71ae3d1c419994425d0a0d8b
Zhangyuliang <zhangyl@maycur.com>
2023-09-14 14:06:23 +0800

    增加shardingsphere分表

--- src/main/java/com/zyl/mypro/controller/IndexController.java
+++ src/main/java/com/zyl/mypro/controller/IndexController.java
@@ -1,23 +1,12 @@
 package com.zyl.mypro.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
+import com.zyl.mypro.bean.Cost;
+import com.zyl.mypro.service.CostService;
+import com.zyl.mypro.util.JwtUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.DispatcherServlet;
@@ -25,18 +14,20 @@ import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
 import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
-import com.zyl.mypro.bean.Cost;
-import com.zyl.mypro.service.CostService;
-import com.zyl.mypro.util.JwtUtil;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.*;
 
 @RestController
 public class IndexController {
+	private static final Logger log = LoggerFactory.getLogger(IndexController.class);
 	int count = 0;
 	@Autowired(required = false)
 	CostService costService;
 	
 	//模拟beta用户
-	private Set<String> betaSet = new HashSet<>();
+	private final Set<String> betaSet = new HashSet<>();
 	{
 		betaSet.add("zyl");
 		betaSet.add("wqc");
@@ -56,8 +47,7 @@ public class IndexController {
 	}
     @GetMapping("/hello/{name}")
     @ResponseBody
-    public HelloVO say(HttpServletRequest request, 
-    		HttpServletResponse response, 
+    public HelloVO say(HttpServletRequest request,
     		@PathVariable("name") String name){
     	String beta = request.getHeader("beta");
     	HelloVO vo = new HelloVO();
@@ -66,6 +56,20 @@ public class IndexController {
     	vo.setBeta(beta);
     	return vo;
     }
+
+	@GetMapping("/shardingsphereTest")
+	@ResponseBody
+	public Cost shardingsphereTest(HttpServletRequest request){
+
+		String id = request.getParameter("id");
+		Cost cost = costService.select(Integer.parseInt(id));
+
+		if(cost == null) {
+			log.debug("查询Cost结果为空");
+		}
+		return cost;
+	}
+
     @GetMapping("/login")
     @ResponseBody
     public HelloVO login(HttpServletRequest request, 
@@ -89,40 +93,36 @@ public class IndexController {
     	try {
 			response.sendRedirect("http://zyl.com/a.html?p=" + pToken);
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.error("", e);
 		}
     	return vo;
     }
 
     /**
      * 测试浏览器redirect
-     * @param response
      */
     @GetMapping("/redirect")
     public void redirect(HttpServletResponse response){
     	try {
 			response.sendRedirect("https://baidu.com/");
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.error("", e);
 		}
     }
-    
-    /**
-     * 
-     * @param request
-     * @return
-     */
+
     @RequestMapping("/getAllUrl")                                   
     @ResponseBody
     public Set<String> getAllUrl(HttpServletRequest request) {
-        Set<String> result = new HashSet<String>();
+        Set<String> result = new HashSet<>();
         WebApplicationContext wc = (WebApplicationContext) request.getAttribute(DispatcherServlet.WEB_APPLICATION_CONTEXT_ATTRIBUTE);
         RequestMappingHandlerMapping bean = wc.getBean(RequestMappingHandlerMapping.class);
         Map<RequestMappingInfo, HandlerMethod> handlerMethods = bean.getHandlerMethods();
         for (RequestMappingInfo rmi : handlerMethods.keySet()) {
             PatternsRequestCondition pc = rmi.getPatternsCondition();
-            Set<String> pSet = pc.getPatterns();
-            result.addAll(pSet);
+			if(pc != null) {
+				Set<String> pSet = pc.getPatterns();
+				result.addAll(pSet);
+			}
         }
         return result;
     }
