91dc0f431e160d09529fa942e17e0446dc87bcf1
Zhangyuliang <zhangyl@maycur.com>
2023-09-15 09:34:08 +0800

    自定义shardingsphere分表策略TenementShardingAlgorithm完善配置

--- src/main/java/com/zyl/mypro/sharding/TenementShardingAlgorithm.java
+++ src/main/java/com/zyl/mypro/sharding/TenementShardingAlgorithm.java
@@ -1,28 +1,42 @@
 package com.zyl.mypro.sharding;
 
+import com.google.common.base.Splitter;
+import org.apache.commons.lang.StringUtils;
 import org.apache.shardingsphere.sharding.api.sharding.standard.PreciseShardingValue;
 import org.apache.shardingsphere.sharding.api.sharding.standard.RangeShardingValue;
 import org.apache.shardingsphere.sharding.api.sharding.standard.StandardShardingAlgorithm;
 
-import java.util.Collection;
-import java.util.Properties;
+import java.util.*;
 import java.util.zip.CRC32;
 
 public final class TenementShardingAlgorithm implements StandardShardingAlgorithm<String> {
+
+    private Properties props;
+    private Set<String> specialTenementSet;
+
+    private int modNumber = 1;
+
     @Override
     public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<String> shardingValue) {
+
         String value = shardingValue.getValue();
-        if(value.equals("T001")) {
-            String actuallyTableName = shardingValue.getLogicTableName() + "_" + "zyl";
+
+        String actuallyTableName;
+        //大租户独占表
+        if(specialTenementSet.contains(value)) {
+            actuallyTableName = shardingValue.getLogicTableName() + "_" + value;
             return actuallyTableName;
         }
+        //普通租户分表
         CRC32 crc32 = new CRC32();
         crc32.update(value.getBytes());
-        long suffix = crc32.getValue() % 2;
-        String actuallyTableName = shardingValue.getLogicTableName() + "_" + suffix;
+        //抽配置
+        long suffix = crc32.getValue() % modNumber;
+        actuallyTableName = shardingValue.getLogicTableName() + "_" + suffix;
         if (availableTargetNames.contains(actuallyTableName)) {
             return actuallyTableName;
         }
+
         return null;
     }
 
@@ -33,11 +47,22 @@ public final class TenementShardingAlgorithm implements StandardShardingAlgorith
 
     @Override
     public Properties getProps() {
-        return null;
+        return props;
     }
 
     @Override
     public void init(Properties properties) {
+
+        this.props = properties;
+
+        String specialTenement = props.getProperty("special-tenement");
+        List<String> tenementList = Splitter.on(",").trimResults().omitEmptyStrings().splitToList(specialTenement);
+        specialTenementSet = new HashSet<>(tenementList);
+
+        String modNum = props.getProperty("mod-number");
+        if(StringUtils.isNotBlank(modNum)) {
+            modNumber = Integer.parseInt(modNum.trim());
+        }
 
     }
 
