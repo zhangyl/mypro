a3522145ba04ebba35072bbb13e539b925316ca9
zhangyl <zhangyl@maycur.com>
2024-07-27 11:46:02 +0800

    mybatis日志拦截demo（SqlStatementInterceptor）

--- src/main/java/com/zyl/mypro/aop/SqlStatementInterceptor.java
+++ src/main/java/com/zyl/mypro/aop/SqlStatementInterceptor.java
@@ -6,11 +6,22 @@ import com.alibaba.druid.sql.SQLUtils;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.ast.statement.SQLUpdateStatement;
 import com.mysql.cj.jdbc.ClientPreparedStatement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.executor.statement.StatementHandler;
 import org.apache.ibatis.logging.jdbc.PreparedStatementLogger;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
 import org.apache.ibatis.plugin.*;
 import org.apache.ibatis.reflection.MetaObject;
 import org.apache.ibatis.reflection.SystemMetaObject;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.type.TypeHandlerRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -18,14 +29,15 @@ import java.lang.reflect.Proxy;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.Statement;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.text.DateFormat;
+import java.util.*;
+import java.util.regex.Matcher;
 
 @Component
-@Intercepts({@Signature(type = StatementHandler.class, method = "update", args = {Statement.class}),
-    @Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class}),
-    @Signature(type = StatementHandler.class, method = "batch", args = {Statement.class})})
+@Intercepts({@Signature(type = Executor.class, method = "update", args = {MappedStatement.class,Object.class}),
+//    @Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class}),
+//    @Signature(type = StatementHandler.class, method = "batch", args = {Statement.class})
+})
 
 
 public class SqlStatementInterceptor implements Interceptor {
@@ -34,47 +46,38 @@ public class SqlStatementInterceptor implements Interceptor {
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        String origSql = "";
-        Object result = realTarget(invocation.getArgs()[0]);
 
-        if(result instanceof PreparedStatementLogger) {
-            PreparedStatementLogger preparedStatementLogger = (PreparedStatementLogger) result;
-            PreparedStatement preparedStatement = preparedStatementLogger.getPreparedStatement();
-            if (preparedStatement instanceof DruidPooledPreparedStatement) {
-                DruidPooledPreparedStatement druidPooledPreparedStatement = (DruidPooledPreparedStatement) preparedStatement;
-                PreparedStatement rawStatement = druidPooledPreparedStatement.getRawStatement();
-                String origSqlClassStr = "";
-                if (rawStatement instanceof ClientPreparedStatement) {
-                    ClientPreparedStatement statement = (ClientPreparedStatement) rawStatement;
-                    origSql = statement.asSql();
-                } else if (rawStatement instanceof PreparedStatementProxyImpl) {
-                    PreparedStatementProxyImpl statement = (PreparedStatementProxyImpl) rawStatement;
-                    origSqlClassStr = statement.getRawObject().toString();
-                }
-                System.out.println(origSql);
-
-                SQLStatement sqlStatement = SQLUtils.parseSingleStatement(origSql, "mysql");
-
-                if(sqlStatement instanceof SQLUpdateStatement) {
-                    System.out.println("执行的SQL result=" + result);
-                    System.out.println("执行的SQL: \n" + origSql);
-                    System.out.println("执行的SQL sqlStatement: \n" + sqlStatement);
-
-                    List<Map<String, Object>> beforeImageList = sqlStatementProxyHandler.beforeImage((SQLUpdateStatement)sqlStatement);
-                    System.out.println("执行SQL前数据: \n" + beforeImageList);
-
-                    Object o = invocation.proceed();
-
-                    List<Map<String, Object>> afterImageList = sqlStatementProxyHandler.afterImage((SQLUpdateStatement)sqlStatement, null, beforeImageList);
-                    System.out.println("执行SQL后数据: \n" + afterImageList);
-
-                    return o;
-                }
-            }
-
+        final Object[] args = invocation.getArgs();
+        MappedStatement ms = (MappedStatement) args[0];
+        Object parameter = null;
+        if (invocation.getArgs().length > 1) {
+            parameter = invocation.getArgs()[1];
         }
-//        String origSql = statementHandler.getBoundSql().getSql();
+        String sqlId = ms.getId();
+        Configuration configuration = ms.getConfiguration();
+        BoundSql boundSql = ms.getBoundSql(parameter);
 
+        String origSql = realSql(configuration, boundSql);
+
+        SQLStatement sqlStatement = SQLUtils.parseSingleStatement(origSql, "mysql");
+
+        if(sqlStatement instanceof SQLUpdateStatement) {
+            Object result = realTarget(invocation.getArgs()[0]);
+
+            System.out.println("执行的SQL result=" + result);
+            System.out.println("执行的SQL: \n" + origSql);
+            System.out.println("执行的SQL sqlStatement: \n" + sqlStatement);
+
+            List<Map<String, Object>> beforeImageList = sqlStatementProxyHandler.beforeImage((SQLUpdateStatement)sqlStatement);
+            System.out.println("执行SQL前数据: \n" + beforeImageList);
+
+            Object o = invocation.proceed();
+
+            List<Map<String, Object>> afterImageList = sqlStatementProxyHandler.afterImage((SQLUpdateStatement)sqlStatement, null, beforeImageList);
+            System.out.println("执行SQL后数据: \n" + afterImageList);
+
+            return o;
+        }
 
         Object o = invocation.proceed();
         // 继续执行后续操作
@@ -100,5 +103,65 @@ public class SqlStatementInterceptor implements Interceptor {
     @Override
     public void setProperties(Properties properties) {
         // 可以接收配置的属性
+    }
+
+    // 如果参数是String，则添加单引号， 如果是日期，则转换为时间格式器并加单引号； 对参数是null和不是null的情况作了处理
+    private static String getParameterValue(Object obj) {
+        String value = null;
+        if (obj instanceof String) {
+            value = "'" + obj + "'";
+        } else if (obj instanceof Date) {
+            DateFormat formatter = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.CHINA);
+            value = "'" + formatter.format(new Date()) + "'";
+        } else {
+            if (obj != null) {
+                value = obj.toString();
+            } else {
+                value = "";
+            }
+
+        }
+        return value;
+    }
+
+    /**
+     * 进行？的替换过的真正sql
+     * @param configuration
+     * @param boundSql
+     * @return
+     */
+    public static String realSql(Configuration configuration, BoundSql boundSql) {
+        // 获取参数
+        Object parameterObject = boundSql.getParameterObject();
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        // sql语句中多个空格都用一个空格代替
+        String sql = boundSql.getSql().replaceAll("[\\s]+", " ");
+        if (CollectionUtils.isNotEmpty(parameterMappings) && parameterObject != null) {
+            // 获取类型处理器注册器，类型处理器的功能是进行java类型和数据库类型的转换
+            TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+            // 如果根据parameterObject.getClass(）可以找到对应的类型，则替换
+            if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
+                sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(getParameterValue(parameterObject)));
+            } else {
+                // MetaObject主要是封装了originalObject对象，提供了get和set的方法用于获取和设置originalObject的属性值,
+                // 主要支持对JavaBean、Collection、Map三种类型对象的操作
+                MetaObject metaObject = configuration.newMetaObject(parameterObject);
+                for (ParameterMapping parameterMapping : parameterMappings) {
+                    String propertyName = parameterMapping.getProperty();
+                    if (metaObject.hasGetter(propertyName)) {
+                        Object obj = metaObject.getValue(propertyName);
+                        sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(getParameterValue(obj)));
+                    } else if (boundSql.hasAdditionalParameter(propertyName)) {
+                        // 该分支是动态sql
+                        Object obj = boundSql.getAdditionalParameter(propertyName);
+                        sql = sql.replaceFirst("\\?", Matcher.quoteReplacement(getParameterValue(obj)));
+                    } else {
+                        // 打印出缺失，提醒该参数缺失并防止错位
+                        sql = sql.replaceFirst("\\?", "缺失");
+                    }
+                }
+            }
+        }
+        return sql;
     }
 }
